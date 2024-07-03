7a9b6da5f0634503d413066c72cf2e9fdc301fb2
rashmitha <21e40.rashmitha@sjec.ac.in>
2024-07-03 17:16:42 +0530

    first commit

---
+++ download_nltk_data.py
@@ -0,0 +1,5 @@
+import nltk
+def download_nltk_data():
+    nltk.download('stopwords')
+if __name__ == "__main__":
+    download_nltk_data()
---
+++ dum.py
@@ -0,0 +1,7 @@
+import nltk
+
+# Download the averaged perceptron tagger
+nltk.download('averaged_perceptron_tagger')
+
+# Download WordNet
+nltk.download('wordnet')
